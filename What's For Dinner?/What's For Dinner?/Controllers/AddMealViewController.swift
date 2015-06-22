8b5bc9ea9232d7349980593c2f7e939ec7f2abe2
Camden Voigt <jacksonvoigt@yahoo.com>
2015-06-22 11:14:51 -0500

    Fixed simple protocol error

--- What's For Dinner?/What's For Dinner?/Controllers/AddMealViewController.swift
+++ What's For Dinner?/What's For Dinner?/Controllers/AddMealViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddMealViewController: UIViewController : UITableViewDelegate {
+class AddMealViewController: UIViewController, UITableViewDelegate {
     
     // MARK: IBOutlets
     
